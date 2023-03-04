5bcd2362947342a90e4ede45d898153857bed81a
Marcos Leal <marcosleal@gmail.com>
2023-03-04 20:07:07 +0000

    finished with xray for now

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class UserActivities:
   def run(user_handle):
     model = {
@@ -7,7 +9,7 @@ class UserActivities:
     }
 
     now = datetime.now(timezone.utc).astimezone()
-
+    
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
@@ -20,4 +22,14 @@ class UserActivities:
         'expires_at': (now + timedelta(days=31)).isoformat()
       }]
       model['data'] = results
+
+    # xray
+    subsegment = xray_recorder.begin_subsegment('user_activities')
+    # subsegment = xray_recorder.begin_subsegment('mock_data')
+    # xray
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len(model['data'])
+    }
+    subsegment.put_metadata('key',dict,'namespace')
     return model
